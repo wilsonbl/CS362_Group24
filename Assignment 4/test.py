685319ddb46ca4e686c63299e0ff33cea9cf27d5
Isaac Grossberg <53278539+igrossberg23@users.noreply.github.com>
2020-02-29 23:19:14 -0800

    add unit tests for plant hits and collisions

--- Assignment 4/test.py
+++ Assignment 4/test.py
@@ -45,7 +45,64 @@ class Test(unittest.TestCase):
         #Assert that pea has been added to pea group and is in the proper position
         self.assertEqual(len(main_game.pea_group), 1)
         self.assertEqual(main_game.pea_group.pop().get_pos(), [1 + main_game.pea_direction[0], 1 - main_game.pea_direction[1]])
-
+       
+    def test_plant_lose_health(self):
+        # Create plants
+        plant1 = Plant(1, 1, [1, 1], 1, 1, 10)  # (sun_cost, image, pos, tower_indicator, lane, health)
+        plant2 = Plant(1, 1, [1, 1], 1, 1, 5)
+        plant3 = Plant(1, 1, [1, 1], 1, 1, 1)
+        plant4 = Plant(1, 1, [1, 1], 1, 1, -1)
+        plant5 = Plant(1, 1, [1, 1], 1, 1, 10)
+        
+        # Create zombies
+        zombie1 = Zombie(1, 1, 10)  # (pos, lane, health)
+        zombie2 = Zombie(1, 1, 0)
+        
+        # Have plants get hit
+        plant1.hit(zombie1)
+        plant2.hit(zombie1)
+        plant3.hit(zombie1)
+        plant4.hit(zombie1)
+        plant5.hit(zombie2)
+        
+        # Assert that plant health has been decremented
+        self.assertEqual(plant1.get_health(), 10-1)
+        self.assertEqual(plant2.get_health(), 5-1)
+        self.assertEqual(plant3.get_health(), 1-1)
+        self.assertEqual(plant4.get_health(), -1-1)
+        self.assertEqual(plant5.get_health(), 10-1)
+        
+        # Assert that hit_indicator has been correctly updated
+        self.assertEqual(plant1.hit_indicator, True)
+        self.assertEqual(plant2.hit_indicator, True)
+        self.assertEqual(plant3.hit_indicator, True)
+        self.assertEqual(plant4.hit_indicator, True)
+        self.assertEqual(plant5.hit_indicator, False)
+        
+    def test_plant_collide_with_zombie(self):
+        # Create plants
+        plant1 = Plant(1, 1, [1, 1], 1, 1, 10)
+        
+        # Create zombies
+        zombie1 = Zombie(1, 1, 10)
+        zombie2 = Zombie(41, 1, 10)
+        zombie3 = Zombie(42, 1, 10)
+        zombie4 = Zombie(41, 2, 10)
+        zombie5 = Zombie(42, 2, 10)
+        
+        # Collide zombies and plant
+        collision1 = plant1.collide(zombie1)
+        collision2 = plant1.collide(zombie2)
+        collision3 = plant1.collide(zombie3)
+        collision4 = plant1.collide(zombie4)
+        collision5 = plant1.collide(zombie5)
+        
+        # Assert that collisions were detected correctly
+        self.assertEqual(collision1, True)
+        self.assertEqual(collision2, True)
+        self.assertEqual(collision3, False)
+        self.assertEqual(collision4, False)
+        self.assertEqual(collision5, False)
 
 if __name__ == '__main__':
     unittest.main()
