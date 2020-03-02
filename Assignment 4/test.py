ca324b302cf8041fb4bc90e055e54a691a1ce685
Blair Wilson <wilsonbl@oregonstate.edu>
2020-03-01 19:14:15 -0800

    Added more test cases for test_plant_shoot

--- Assignment 4/test.py
+++ Assignment 4/test.py
@@ -7,21 +7,21 @@ from main_game import Plant
 
 class Test(unittest.TestCase):
     def test_zombie_pea_hit(self):
-        #Create zombies to test on
+        # Create zombies
         zombie1 = Zombie(1, 1, 0)
         zombie2 = Zombie(1, 1, 1)
         zombie3 = Zombie(1, 1, -1)
         zombie4 = Zombie(1, 1, 5)
         zombie5 = Zombie(1, 1, -5)
         
-        #Hit zombies with a pea
+        # Hit zombies with peas
         zombie1.pea_hit()
         zombie2.pea_hit()
         zombie3.pea_hit()
         zombie4.pea_hit()
         zombie5.pea_hit()
 
-        #Assert that health has decreased by 1
+        # Assert that health of each zombie has decreased by 1
         self.assertEqual(zombie1.get_health(), 0-1)
         self.assertEqual(zombie2.get_health(), 1-1)
         self.assertEqual(zombie3.get_health(), -1-1)
@@ -30,21 +30,40 @@ class Test(unittest.TestCase):
 
 
     def test_plant_shoot(self):
-        #Create group of zombies
+        # Create zombies
         zombie_group = set([])
         zombie_group.add(Zombie(1, 1, 10))
+        zombie_group.add(Zombie(1, 2, 10))
+        zombie_group.add(Zombie(1, 2, 10))
+        zombie_group.add(Zombie(1, 4, 10))
+        zombie_group.add(Zombie(1, 5, 10))
+        zombie_group.add(Zombie(1, 5, 10))
 
-        #Create test plants
-        plant1 = Plant(1, 1, [1, 1], 1, 1, 10)   #Lane 1
-        plant2 = Plant(1, 1, [1, 1], 1, 2, 10)   #Lane 2 (no zombie there, so no pea should be added)
+        #Create plants
+        plant1 = Plant(1, 1, [1, 1], 1, 1, 10)   # Lane 1 | 1 zombie, 1 plant. 1 pea should be added
+        plant2 = Plant(1, 1, [1, 1], 1, 2, 10)   # Lane 2 | 2 zombies, 1 plant. 1 pea should be added
+        plant3 = Plant(1, 1, [1, 1], 1, 3, 10)   # Lane 3 | 0 zombies, 1 plant. 0 peas should be added
+        plant4 = Plant(1, 1, [1, 1], 1, 4, 10)   # Lane 4 | 1 zombie, 2 plants. 2 peas should be added
+        plant5 = Plant(1, 1, [1, 1], 1, 4, 10)
+        plant6 = Plant(1, 1, [1, 1], 1, 5, 10)   # Lane 5 | 2 zombies, 2 plants. 2 peas should be added
+        plant7 = Plant(1, 1, [1, 1], 1, 5, 10)
+                                                 # Grand total: 6 peas
 
-        #Shoot pea from plant
+        #Shoot peas from plants
         plant1.shoot(zombie_group)
         plant2.shoot(zombie_group)
+        plant3.shoot(zombie_group)
+        plant4.shoot(zombie_group)
+        plant5.shoot(zombie_group)
+        plant6.shoot(zombie_group)
+        plant7.shoot(zombie_group)
 
-        #Assert that pea has been added to pea group and is in the proper position
-        self.assertEqual(len(main_game.pea_group), 1)
-        self.assertEqual(main_game.pea_group.pop().get_pos(), [1 + main_game.pea_direction[0], 1 - main_game.pea_direction[1]])
+        #Assert that correct number of peas have been added to pea group
+        self.assertEqual(len(main_game.pea_group), 6)
+
+        #Assert that each pea is in the proper position
+        while len(main_game.pea_group) > 0:
+            self.assertEqual(main_game.pea_group.pop().get_pos(), [1 + main_game.pea_direction[0], 1 - main_game.pea_direction[1]])
        
     def test_plant_lose_health(self):
         # Create plants
